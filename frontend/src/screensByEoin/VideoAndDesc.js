47d285bc1921638856cd3da797c2ebc6de532a9c
eoinlynchcodes <eoin95lynch@gmail.com>
2020-12-07 17:54:56 +0000

    Homepage works.

--- frontend/src/screensByEoin/VideoAndDesc.js
+++ frontend/src/screensByEoin/VideoAndDesc.js
@@ -6,21 +6,22 @@ import firewoodgraphic from "../imagesByEoin/firewoodgraphic.png";
 function VideoAndDesc() {
   return (
     <div className="videoAndDescription">
-
       <div className="left33">
         <div className="textToSquare">
-          <h1>Firewood, delivered...</h1>
+          <h1>Firewood. Delivered.</h1>
+          <h5>Order online and get firewood delivered in Westmeath. </h5>
+        <div>
+          <h3>Order now</h3>
+        </div>
         </div>
         <div className="firewoodInfographicDiv">
-        <img src={firewoodgraphic} />
+          <img src={firewoodgraphic} />
         </div>
       </div>
 
       <div className="right66">
-  
         <img src={stoveburning} />
       </div>
-
     </div>
   );
 }
